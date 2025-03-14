37ba32c207ae81fa2cf767af4cdf488cacb78894
Balasanthoshini M <23csea05balasanthoshini@gmail.com>
2025-03-14 20:17:30 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Activities from "./components/Activities";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Activities />
+    </div>
+  );
+};
+
+export default App;
